7bda6a7e34e04a47365a567fb381214f2b7a9804
visualstudio <visualstudio@122-PC39>
2019-02-01 20:28:46 -0500

    c0732136

--- C0732136/Program.cs
+++ C0732136/Program.cs
@@ -10,6 +10,59 @@ namespace C0732136
     {
         static void Main(string[] args)
         {
+            //countryside c = new countryside();
+            //c.run();
+            (new learningexp()).run();
+
         }
     }
+    class village
+    {
+        public bool isAstrildeHere;
+        public village nextvillage;
+        public village prevvillage;
+        public String villagename;
+
+
+    }
+    class learningexp
+    {
+        public void run()
+        {
+            village toronto;
+            toronto = new village();
+            village a, b, c;
+            toronto.villagename = "version a";
+            a = toronto;
+            Console.WriteLine(a.villagename);
+            toronto = new village();
+            toronto.villagename = "version b";
+            b = toronto;
+            Console.WriteLine(b.villagename);
+            toronto = new village();
+            toronto.villagename = "version c";
+            c = toronto;
+            Console.WriteLine(c.villagename);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class countryside
+    {
+        village maple;
+        village toronto;
+        village ajax;
+
+        public void run()
+        {
+            maple = new village();
+            maple.villagename = "toronto";
+            maple.nextvillage = toronto;
+
+        }
+
+    }
+
 }
